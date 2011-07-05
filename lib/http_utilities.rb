1e245b2a6fca4a5aeb534e6fff7b0fbec1b0dcf6
Sebastian Johnsson <sebastian@agiley.se>
2011-07-05 16:17:29 +0200

    Added support for initializer setup

--- lib/http_utilities.rb
+++ lib/http_utilities.rb
@@ -2,6 +2,13 @@
 module HttpUtilities
   require File.join(File.dirname(__FILE__), 'http_utilities/railtie') if defined?(Rails)
   
+  mattr_accessor :default_http_library
+  @@default_http_library = :net_http
+
+  def self.setup
+    yield self
+  end
+  
   require File.join(File.dirname(__FILE__), 'http_utilities/http/net_http')
   require File.join(File.dirname(__FILE__), 'http_utilities/http/open_uri')
   require File.join(File.dirname(__FILE__), 'http_utilities/http/curb')
