45c2b63dd7b4b4cad3581e110e002bb10f0c8939
Sebastian Johnsson <sebastian@agiley.se>
2011-07-05 16:30:38 +0200

    Bug fixes

--- lib/generators/http_utilities/http_utilities_generator.rb
+++ lib/generators/http_utilities/http_utilities_generator.rb
@@ -1,12 +1,22 @@
 module HttpUtilities
   module Generators
-    class HttpUtilitiesGenerator < Rails::Generators::NamedBase
+    class HttpUtilitiesGenerator < Rails::Generators::Base
       namespace "http_utilities"
-      source_root File.expand_path("../templates", __FILE__)
-
-      desc "Generates a proxy model and migration file, as well as generating a configuration file."
-
+      source_root File.expand_path("../../templates", __FILE__)
+      
+      class_option :orm
       hook_for :orm
+      
+      desc "Creates a HttpUtilities-initializer."
+      def copy_initializer
+        template "http_utilities.rb", "config/initializers/http_utilities.rb"
+      end
+      
+      desc "Copies User Agents"
+      def copy_user_agents
+        template "user_agents.yml", "config/http_utilities/user_agents.yml"
+      end
+      
     end
   end
 end
--- lib/generators/http_utilities/orm_helpers.rb
+++ lib/generators/http_utilities/orm_helpers.rb
@@ -1,4 +1,4 @@
-module Devise
+module HttpUtilities
   module Generators
     module OrmHelpers
       
