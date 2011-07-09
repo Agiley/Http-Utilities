38238b5dbeb376a4cd4bfd81e509a3e4255c3fa0
Sebastian Johnsson <sebastian@agiley.se>
2011-07-09 11:20:22 +0200

    Minor bug fix

--- lib/generators/http_utilities/http_utilities_generator.rb
+++ lib/generators/http_utilities/http_utilities_generator.rb
@@ -10,7 +10,7 @@ module HttpUtilities
       desc "Copies an initializer, a .yml-file containing user-agents as well as a proxy model."
       
       def copy_initializer
-        template "http_utilities.rb", "config/initializers/http_utilities.rb" unless file_exists?("config/initializers/http_utilities.rb")
+        template "http_utilities.rb", "config/initializers/http_utilities.rb"
       end
       
       def copy_user_agents
