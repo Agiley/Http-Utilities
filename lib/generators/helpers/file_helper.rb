8f6ed54a7e249d6ae5195e61a1956cc5b596f936
Sebastian Johnsson <sebastian@agiley.se>
2011-07-09 11:43:42 +0200

    Minor bugfix

--- lib/generators/helpers/file_helper.rb
+++ lib/generators/helpers/file_helper.rb
@@ -5,7 +5,7 @@ module HttpUtilities
       private
 
       def copy_dir(source, destination)
-        root_dir=File.join(source_root, source)
+        root_dir=File.join(self.source_root, source)
         Dir[File.join(root_dir, "**/*")].each do |file|
           relative = file.gsub(/^#{root_dir}\//, '')
           copy_file file, File.join(Rails.root, destination, relative) if File.file?(file)
