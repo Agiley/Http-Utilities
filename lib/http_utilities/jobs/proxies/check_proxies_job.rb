ee21349e06e017f7ef3174cefff1bc55dfacd303
Sebastian Johnsson <sebastian@agiley.se>
2011-07-12 11:39:23 +0200

    Minor update for proxies job

--- lib/http_utilities/jobs/proxies/check_proxies_job.rb
+++ lib/http_utilities/jobs/proxies/check_proxies_job.rb
@@ -5,7 +5,7 @@ module HttpUtilities
         @queue = :proxies
 
         def self.perform(protocol = :all, proxy_type = :all)
-          HttpUtilities::Proxies::ProxyChecker.new.check_proxies(protocol, proxy_type)
+          HttpUtilities::Proxies::ProxyChecker.new.check_proxies(protocol.to_sym, proxy_type.to_sym)
         end
       end
     end
