b4cc9d9e4c5cc7255e89dfdf80c1ad2f53d7fbe1
Sebastian Johnsson <sebastian@agiley.se>
2012-01-27 01:09:26 +0100

    Removing present? (rails-dep)

--- lib/http_utilities/http/format.rb
+++ lib/http_utilities/http/format.rb
@@ -6,19 +6,19 @@ module HttpUtilities
     module Format
       
       def as_html
-        self.parsed_body = (self.body && self.body.present?) ? Nokogiri::HTML(self.body, nil, "utf-8") : nil
+        self.parsed_body = (self.body && self.body != "") ? Nokogiri::HTML(self.body, nil, "utf-8") : nil
       end
 
       def as_xml
-        self.parsed_body = (self.body && self.body.present?) ? Nokogiri::XML(self.body, nil, "utf-8") : nil
+        self.parsed_body = (self.body && self.body != "") ? Nokogiri::XML(self.body, nil, "utf-8") : nil
       end
       
       def as_multi_xml
-        self.parsed_body = (self.body && self.body.present?) ? MultiXml.parse(self.body) : nil
+        self.parsed_body = (self.body && self.body != "") ? MultiXml.parse(self.body) : nil
       end
 
       def as_json
-        self.parsed_body = (self.body && self.body.present?) ? self.body.to_json : nil
+        self.parsed_body = (self.body && self.body != "") ? self.body.to_json : nil
       end
       
     end
