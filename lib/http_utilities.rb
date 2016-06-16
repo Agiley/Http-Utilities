4fd5350b9f8e3363130a4ee1a76b6abf756fb560
Sebastian Johnsson <sebastian.johnsson@gmail.com>
2016-06-16 16:55:01 +0700

    Release 1.2.6.3

--- lib/http_utilities.rb
+++ lib/http_utilities.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 module HttpUtilities
-  VERSION = "1.2.6.2"
+  VERSION = "1.2.6.3"
   
   require File.join(File.dirname(__FILE__), 'http_utilities/railtie') if defined?(Rails)
 
