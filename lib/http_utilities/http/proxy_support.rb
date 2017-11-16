bc23ce3f3c10272dd40b7dc56f9fe913667c45ef
Sebastian <sebastian.johnsson@gmail.com>
2017-11-16 21:35:43 +0100

    Release 1.3.5

--- lib/http_utilities/http/proxy_support.rb
+++ lib/http_utilities/http/proxy_support.rb
@@ -3,65 +3,81 @@ module HttpUtilities
     module ProxySupport
 
       def set_proxy_options(options = {})
-        use_proxy                 =   options.fetch(:use_proxy, false)
-        specific_proxy            =   options.fetch(:proxy, nil)
-        proxy_host                =   options.fetch(:proxy_host, nil)
-        proxy_port                =   options.fetch(:proxy_port, nil)
-        proxy_username            =   options.fetch(:proxy_username, nil)
-        proxy_password            =   options.fetch(:proxy_password, nil)
-        proxy_credentials         =   options.fetch(:proxy_credentials, nil)
-        proxy_type                =   options.fetch(:proxy_type, :all)
-        proxy_protocol            =   options.fetch(:proxy_protocol, :all)
+        proxy_opts                    =   options.fetch(:proxy, nil)
         
-        if use_proxy || specific_proxy
-          self.proxy            ||=   {}
+        if proxy_opts
+          self.proxy                ||=   {}
           
-          if specific_proxy && specific_proxy.is_a?(String)
-            specific_proxy        =   specific_proxy.gsub(/^http(s)?:\/\//i, "")
-            parts                 =   specific_proxy.split(":")
-
-            if parts.size.eql?(2)
-              self.proxy[:host]   =   parts.first
-              self.proxy[:port]   =   parts.second.to_i
-            end
-
-          elsif proxy_host && proxy_port
-            self.proxy[:host]     =   proxy_host
-            self.proxy[:port]     =   proxy_port
-
-          elsif proxy_model_defined?
-            if specific_proxy && specific_proxy.is_a?(::Proxy)
-              proxy_object        =   specific_proxy
-            else
-              proxy_object        =   ::Proxy.get_random_proxy(protocol: proxy_protocol, proxy_type: proxy_type)
-            end
+          if proxy_opts.is_a?(String)
+            set_from_string(proxy_opts)
             
-            #log(:info, "[HttpUtilities::Http::ProxySupport] - Randomized Proxy object: #{proxy_object.inspect}")
-
-            if proxy_object
-              self.proxy[:host]   =   proxy_object.host
-              self.proxy[:port]   =   proxy_object.port
-              proxy_username      =   !proxy_object.username.to_s.empty? ? proxy_object.username : nil
-              proxy_password      =   !proxy_object.password.to_s.empty? ? proxy_object.password : nil
-            end
+          elsif proxy_opts.is_a?(Hash)
+            set_from_hash(proxy_opts)
+            
+          elsif proxy_model_defined? && proxy_opts.is_a?(::Proxy)
+            set_from_object(proxy_opts)
           end
+        end
+      end
+      
+      def set_from_string(proxy_opts)
+        proxy_opts                    =   proxy_opts.gsub(/^http(s)?:\/\//i, "")
+        parts                         =   proxy_opts.split(":")
+
+        if parts.size.eql?(2)
+          self.proxy[:host]           =   parts.first
+          self.proxy[:port]           =   parts.second.to_i
+        end
+      end
+      
+      def set_from_hash(proxy_opts)
+        host                          =   proxy_opts.fetch(:host, nil)
+        port                          =   proxy_opts.fetch(:port, nil)
+        
+        username                      =   proxy_opts.fetch(:username, nil)
+        password                      =   proxy_opts.fetch(:password, nil)
+        credentials                   =   proxy_opts.fetch(:credentials, nil)
+        
+        randomize                     =   proxy_opts.fetch(:randomize, true)
+        type                          =   proxy_opts.fetch(:type, :all)
+        protocol                      =   proxy_opts.fetch(:protocol, :all)
+        
+        if randomize && proxy_model_defined?
+          proxy_object                =   ::Proxy.get_random_proxy(protocol: protocol, proxy_type: type)
+          set_from_object(proxy_object)
+        else
+          if host && port
+            self.proxy[:host]         =   host
+            self.proxy[:port]         =   port
+            
+            set_credentials(username, password)
+          end
+        end
+      end
+      
+      def set_from_object(proxy_object)
+        if proxy_object
+          self.proxy[:host]           =   proxy_object.host
+          self.proxy[:port]           =   proxy_object.port
+          username                    =   !proxy_object.username.to_s.empty? ? proxy_object.username : nil
+          password                    =   !proxy_object.password.to_s.empty? ? proxy_object.password : nil
           
-          set_proxy_credentials(proxy_username, proxy_password, proxy_credentials)
+          set_credentials(username, password)
         end
       end
 
-      def set_proxy_credentials(proxy_username, proxy_password, proxy_credentials)
-        if proxy_username && proxy_password
-          self.proxy[:username]       =   proxy_username
-          self.proxy[:password]       =   proxy_password
+      def set_credentials(username, password, credentials = {})
+        if username && password
+          self.proxy[:username]       =   username
+          self.proxy[:password]       =   password
 
-        elsif proxy_credentials
-          if proxy_credentials.is_a?(Hash)
-            self.proxy[:username]     =   proxy_credentials[:username]
-            self.proxy[:password]     =   proxy_credentials[:password]
+        elsif credentials
+          if credentials.is_a?(Hash)
+            self.proxy[:username]     =   credentials[:username]
+            self.proxy[:password]     =   credentials[:password]
 
-          elsif (proxy_credentials.is_a?(String))
-            parts                     =   proxy_credentials.split(":")
+          elsif (credentials.is_a?(String))
+            parts                     =   credentials.split(":")
 
             if parts && parts.any? && parts.size == 2
               self.proxy[:username]   =   parts.first
@@ -72,19 +88,19 @@ module HttpUtilities
       end
       
       def proxy_model_defined?
-        defined                     =   Module.const_get("Proxy").is_a?(Class) rescue false
-        defined                     =   (defined && ::Proxy.respond_to?(:get_random_proxy))
+        defined                       =   Module.const_get("Proxy").is_a?(Class) rescue false
+        defined                       =   (defined && ::Proxy.respond_to?(:get_random_proxy))
         
         return defined
       end
       
       def generate_proxy_options
-        proxy_options               =   {}
+        proxy_options                 =   {}
         
         if self.proxy && !self.proxy[:host].to_s.empty? && !self.proxy[:port].to_s.empty?
-          proxy_options[:uri]       =   "http://#{self.proxy[:host]}:#{self.proxy[:port]}"
-          proxy_options[:user]      =   self.proxy[:username] if !self.proxy[:username].to_s.empty?
-          proxy_options[:password]  =   self.proxy[:password] if !self.proxy[:password].to_s.empty?
+          proxy_options[:uri]         =   "http://#{self.proxy[:host]}:#{self.proxy[:port]}"
+          proxy_options[:user]        =   self.proxy[:username] if !self.proxy[:username].to_s.empty?
+          proxy_options[:password]    =   self.proxy[:password] if !self.proxy[:password].to_s.empty?
         end
         
         return proxy_options
